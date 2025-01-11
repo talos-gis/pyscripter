442c98a6d376393b8e246388071a78b1a97d1916
Idan Miara <idan@miara.com>
2025-01-11 14:51:53 +0200

    frmPyIDEMain.pas - unhide InternalInterpreter

--- Source/frmPyIDEMain.pas
+++ Source/frmPyIDEMain.pas
@@ -2818,6 +2818,9 @@ begin
   MaskFPUExceptions(PyIDEOptions.MaskFPUExceptions);
 
   // Set Python engine
+  PyIDEOptions.InternalInterpreterHidden := False;
+  PyIDEOptions.PythonEngineType := peInternal;
+
   actPythonInternal.Visible := not PyIDEOptions.InternalInterpreterHidden;
   if not actPythonInternal.Visible and
      (PyIDEOptions.PythonEngineType = peInternal)

2e058e210548bf2b71025d6525cbe1aafbc8dc2c
Idan Miara <idan@miara.com>
2025-01-15 21:22:12 +0200

    add an example for registering a function on the internal interpreter

--- Source/frmPyIDEMain.pas
+++ Source/frmPyIDEMain.pas
@@ -693,7 +693,8 @@ uses
   uEditAppIntfs,
   cFileTemplates,
   cPySupportTypes,
-  cPyControl;
+  cPyControl,
+  cInternalPython;
 
 const
   WM_FINDDEFINITION  = WM_USER + 100;
@@ -1516,11 +1517,39 @@ end;
 type
   TTBCustomItemAccess = class(TTBCustomItem);
 
+
+function py42(self, args: PPYObject): PPYObject; cdecl;
+var
+  PyEngine: TPythonEngine;
+begin
+  PyEngine := GetPythonEngine;
+  Result := pyEngine.PyLong_FromLong(42);
+end;
+
+procedure DoRegister(PyEngine: TPythonEngine);
+{
+from mymodule import py42
+print(py42())
+print(py42.__doc__)
+}
+var
+  PyModule: TPythonModule;
+begin
+  PyModule := TPythonModule.Create(nil);
+
+  PyModule.Name := 'PyModule';
+  PyModule.ModuleName := 'mymodule';
+  PyModule.Engine := PyEngine;
+  PyModule.AddMethod(PAnsiChar('py42'),py42,PAnsiChar('returns forty two'));
+end;
+
 procedure TPyIDEMainForm.FormCreate(Sender: TObject);
 var
   TabHost: TJvDockTabHostForm;
   LocalOptionsFileName: string;
 begin
+  cInternalPython.CreateExternalModules := DoRegister;
+
   // Shell Images
   FShellImages := TCommonVirtualImageList.Create(Self);
   TCommonVirtualImageList(FShellImages).SourceImageList := SmallSysImages;
